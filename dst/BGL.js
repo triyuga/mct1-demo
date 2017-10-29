f86aae3458d62d5cde7eb098111edf291d0c7a78
Tim Marwick <tim@magikcraft.io>
2017-10-29 16:49:15 +1000

    up

--- dst/BGL.js
+++ dst/BGL.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// import { IBar } from 'magikcraft-lore-ui-bar/dst';
+var log = require("./old/util/log");
 var Bar = require("./Bar");
 var magik = magikcraft.io;
 var BGL_BAR_KEY = 'mct1.bar.bgl';
@@ -24,18 +26,17 @@ var BGLProvider = (function () {
         magik.playerMap.put(BGL_BAR_KEY, bar);
     };
     BGLProvider.prototype.set = function (bgl) {
-        // log.info(`BGL.set(${bgl})`);
-        // if (magik.playerMap.containsKey(BGL_BAR_KEY)) {
-        // 	let _bar = magik.playerMap.get(BGL_BAR_KEY);
-        // 	_bar.destroy();
-        // }
-        // const bar = Bar.bar()
-        // 	.text(`BGL: ${bgl}`)
-        // 	.color(Bar.color.GREEN)
-        // 	.style(Bar.style.NOTCHED_20)
-        // 	.progress(bgl)
-        // 	.show();
-        var bar = magik.playerMap.get(BGL_BAR_KEY);
+        log.info("BGL.set(" + bgl + ")");
+        if (magik.playerMap.containsKey(BGL_BAR_KEY)) {
+            var _bar = magik.playerMap.get(BGL_BAR_KEY);
+            _bar.destroy();
+        }
+        var bar = Bar.bar()
+            .text("BGL: " + bgl)
+            .color(Bar.color.GREEN)
+            .style(Bar.style.NOTCHED_20)
+            .progress(bgl)
+            .show();
         magik.playerMap.put(BGL_BAR_KEY, bar);
     };
     return BGLProvider;
