83025635eaf7e146ea8438fa07877259ad67c478
Tim Marwick <tim@magikcraft.io>
2017-10-29 18:08:20 +1000

    up

--- dst/index.js
+++ dst/index.js
@@ -76,11 +76,13 @@ function listenConsume() {
         var foodType = event.getType();
         magik.dixit("you ate a " + foodType + "!");
         // magik.dixit('event: ' + JSON.stringify(event));
+        magik.dixit('1');
         var player = event.getPlayer();
+        magik.dixit('2');
         var playerName = player.getName();
         magik.dixit('playerName: ' + playerName);
-        magik.dixit('player.getHealth(): ' + player.getHealth());
-        magik.dixit('player.getFoodLevel(): ' + player.getFoodLevel());
+        // magik.dixit('player.getHealth(): ' + player.getHealth());
+        // magik.dixit('player.getFoodLevel(): ' + player.getFoodLevel());
     });
 }
 exports.listenConsume = listenConsume;
