49100920f96051edfa133f20cfab0466efcbc4bc
Tim Marwick <tim@magikcraft.io>
2017-10-31 18:22:50 +1000

    up

--- dst/Player.js
+++ dst/Player.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Bar = require("./Bar");
 var Utils_1 = require("./Utils");
 var State_1 = require("./State");
+var fs = require("fs-extra");
 // Read the file, and pass it to your callback
 var magik = magikcraft.io;
 var log = magik.dixit;
@@ -11,54 +12,54 @@ var state = State_1.getState();
 // declare function require(name:string);
 // declare const require: any;
 // const foodList = fs.readFileSync('./men.json, handleJSONFile);
-// const foodList = JSON.parse(fs.readFileSync('./food.json', 'utf8'));
-var foodList = [
-    {
-        "type": "COOKED_CHICKEN",
-        "carbs": 10,
-        "GI": "high"
-    },
-    {
-        "type": "COOKED_FISH",
-        "carbs": 11,
-        "GI": "low"
-    },
-    {
-        "type": "BREAD",
-        "carbs": 14,
-        "GI": "high"
-    },
-    {
-        "type": "COOKIE",
-        "carbs": 30,
-        "GI": "high"
-    },
-    {
-        "type": "APPLE",
-        "carbs": 10,
-        "GI": "low"
-    },
-    {
-        "type": "BAKED_POTATO",
-        "carbs": 25,
-        "GI": "high"
-    },
-    {
-        "type": "PUMPKIN_PIE",
-        "carbs": 31,
-        "GI": "high"
-    },
-    {
-        "type": "MUSHROOM_STEW",
-        "carbs": 14,
-        "GI": "low"
-    },
-    {
-        "type": "BEETROOT",
-        "carbs": 31,
-        "GI": "high"
-    }
-];
+var foodList = JSON.parse(fs.readFileSync('./food.json', 'utf8'));
+// const foodList = [
+// 	{
+// 		"type": "COOKED_CHICKEN",
+// 		"carbs": 10,
+// 		"GI": "high"
+// 	},
+// 	{
+// 		"type": "COOKED_FISH",
+// 		"carbs": 11,
+// 		"GI": "low"
+// 	},
+// 	{
+// 		"type": "BREAD",
+// 		"carbs": 14,
+// 		"GI": "high"
+// 	},
+// 	{
+// 		"type": "COOKIE",
+// 		"carbs": 30,
+// 		"GI": "high"
+// 	},
+// 	{
+// 		"type": "APPLE",
+// 		"carbs": 10,
+// 		"GI": "low"
+// 	},
+// 	{
+// 		"type": "BAKED_POTATO",
+// 		"carbs": 25,
+// 		"GI": "high"
+// 	},
+// 	{
+// 		"type": "PUMPKIN_PIE",
+// 		"carbs": 31,
+// 		"GI": "high"
+// 	},
+// 	{
+// 		"type": "MUSHROOM_STEW",
+// 		"carbs": 14,
+// 		"GI": "low"
+// 	},
+// 	{
+// 		"type": "BEETROOT",
+// 		"carbs": 31,
+// 		"GI": "high"
+// 	}
+// ];
 var Food = {};
 foodList.forEach(function (item) { return Food[item.type] = item; });
 // const inventoryList = JSON.parse(fs.readFileSync('./inventory.json', 'utf8'));
