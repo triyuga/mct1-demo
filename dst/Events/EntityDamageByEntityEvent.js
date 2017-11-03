066ce6af83aa1a7692016c3306f99bbd7048054f
Tim Marwick <tim@magikcraft.io>
2017-11-03 23:57:20 +1000

    up

--- dst/Events/EntityDamageByEntityEvent.js
+++ dst/Events/EntityDamageByEntityEvent.js
@@ -3,17 +3,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var magik = magikcraft.io;
 var log = magik.dixit;
-var CommandCallback = Java.type("io.magikcraft.CommandCallback");
-var EventPriority = Java.type("org.bukkit.event.EventPriority");
-var EventCallback = Java.type("io.magikcraft.EventCallback");
-var EntityDamageByEntityEvent = function (plugin) {
-    plugin.registerEvent(Java.type("org.bukkit.event.entity.EntityDamageByEntityEvent").class, EventPriority.MONITOR, true, new EventCallback({
-        callback: function (event) {
-            log('EntityDamageByEntityEvent');
-            // Emitter.emit('EntityDamageByEntityEvent', event);
-            // const listeners = event.getHandlerList().getRegisteredListeners(magik.getPlugin());
-            // log('listeners: '+ JSON.stringify(listeners));
-        }
-    }));
+var EntityDamageByEntityEvent = function () {
+    log('EntityDamageByEntityEvent');
 };
 exports.default = EntityDamageByEntityEvent;
+// import Emitter from './Emitter';
+// const magik = magikcraft.io;
+// const log = magik.dixit;
+// declare const Java: any;
+// const CommandCallback = Java.type("io.magikcraft.CommandCallback");
+// const EventPriority = Java.type("org.bukkit.event.EventPriority");
+// const EventCallback = Java.type("io.magikcraft.EventCallback");
+// const EntityDamageByEntityEvent = (plugin) => {
+//     plugin.registerEvent(
+//         Java.type("org.bukkit.event.entity.EntityDamageByEntityEvent").class,
+//         EventPriority.MONITOR,
+//         true,
+//         new EventCallback({
+//             callback: function (event: any) {
+//                 log('EntityDamageByEntityEvent');
+//                 // Emitter.emit('EntityDamageByEntityEvent', event);
+//                 // const listeners = event.getHandlerList().getRegisteredListeners(magik.getPlugin());
+//                 // log('listeners: '+ JSON.stringify(listeners));
+//             }
+//         }));	
+// }
+// export default EntityDamageByEntityEvent; 
--- src/events/EntityDamageByEntityEvent.ts
+++ src/events/EntityDamageByEntityEvent.ts
@@ -1,28 +1,37 @@
 // https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/event/player/PlayerInteractEntityEvent.html
 
-import Emitter from './Emitter';
 const magik = magikcraft.io;
 const log = magik.dixit;
-declare const Java: any;
 
-const CommandCallback = Java.type("io.magikcraft.CommandCallback");
-const EventPriority = Java.type("org.bukkit.event.EventPriority");
-const EventCallback = Java.type("io.magikcraft.EventCallback");
-
-const EntityDamageByEntityEvent = (plugin) => {
-    plugin.registerEvent(
-        Java.type("org.bukkit.event.entity.EntityDamageByEntityEvent").class,
-        EventPriority.MONITOR,
-        true,
-        new EventCallback({
-            callback: function (event: any) {
-                log('EntityDamageByEntityEvent');
-                // Emitter.emit('EntityDamageByEntityEvent', event);
-
-                // const listeners = event.getHandlerList().getRegisteredListeners(magik.getPlugin());
-                // log('listeners: '+ JSON.stringify(listeners));
-            }
-        }));	
+const EntityDamageByEntityEvent = () => {
+    log('EntityDamageByEntityEvent');
 }
 
 export default EntityDamageByEntityEvent;
+
+// import Emitter from './Emitter';
+// const magik = magikcraft.io;
+// const log = magik.dixit;
+// declare const Java: any;
+
+// const CommandCallback = Java.type("io.magikcraft.CommandCallback");
+// const EventPriority = Java.type("org.bukkit.event.EventPriority");
+// const EventCallback = Java.type("io.magikcraft.EventCallback");
+
+// const EntityDamageByEntityEvent = (plugin) => {
+//     plugin.registerEvent(
+//         Java.type("org.bukkit.event.entity.EntityDamageByEntityEvent").class,
+//         EventPriority.MONITOR,
+//         true,
+//         new EventCallback({
+//             callback: function (event: any) {
+//                 log('EntityDamageByEntityEvent');
+//                 // Emitter.emit('EntityDamageByEntityEvent', event);
+
+//                 // const listeners = event.getHandlerList().getRegisteredListeners(magik.getPlugin());
+//                 // log('listeners: '+ JSON.stringify(listeners));
+//             }
+//         }));	
+// }
+
+// export default EntityDamageByEntityEvent;
