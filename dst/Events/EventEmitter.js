30e79594728a02ce282ec53edf5e371c6f74e0e6
Tim Marwick <tim@magikcraft.io>
2017-11-03 23:58:26 +1000

    up

--- dst/Events/EventEmitter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var events_1 = require("events");
-var EventEmitter = new events_1.default.EventEmitter();
-exports.default = EventEmitter;
