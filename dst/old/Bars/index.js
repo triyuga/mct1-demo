687925940e6583c4afba55bd331f942d27717a9d
Tim Marwick <tim@magikcraft.io>
2017-11-03 23:27:40 +1000

    up

--- dst/old/Bars/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BGL = require("./BGLBar");
-exports.BGL = BGL;
-var Insulin = require("./InsulinBar");
-exports.Insulin = Insulin;
